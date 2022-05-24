6670930ff1cccf8a92bbafde8013a64913409598
carpet.fate <carpet.fate@gmail.com>
2022-05-24 16:14:30 +0300

    lesson 24 complete

--- 1/js/PersonalMovieDB.js
+++ 1/js/PersonalMovieDB.js
@@ -6,4 +6,78 @@ export default class PersonalMovieDB {
         this.genres = genres;
         this.privat = privat;
     }
+    
+    showMyDB(){
+        if (this.privat == false)
+        {
+            console.log(this);
+        }
+    }
+    
+    writeYourGenres(){
+        for (let i = 1; i <= 3; i++){
+            let input = prompt("Ваш любимый жанр под номером "+ i);
+            // console.log(this.genres[i-i] === (null || '' || 0));
+            let isEmpty = (input == null) || (input == '') || (input == 0);
+
+            while (isEmpty){
+                input = prompt("Ваш любимый жанр под номером "+ i);
+                isEmpty = (input == null) || (input == '') || (input == 0);
+            }
+            this.genres[i-1] = input;
+        }
+
+        this.genres.forEach(element => {
+            console.log(`Любимый жанр ${this.genres.indexOf(element) + 1} - это ${element}`);
+        });
+    }
+    
+    start(){
+        this.count = prompt('Сколько фильмов вы уже посмотрели?', 2);
+    }
+    
+    rememberMyFilms(){
+    
+        for (let i = 0; i < this.count; i++){
+            let answer = this.ask();
+            let name = answer.movieName;
+            let rating = answer.movieRating;      
+    
+            this.movies[name] = rating;
+        }
+    }
+    
+    detectPersonalLevel(){
+        if (this.count < 10){
+            alert("Просмотрено довольно мало фильмов");
+        }
+        else if (this.count >= 10 && this.count < 30){
+            alert("Вы классический зритель");
+        }
+        else if (this.count >= 30){
+            alert("Вы киноман!");
+        }
+        else {
+            alert("Произошла ошибка!");
+        }
+    }
+    
+    ask(){
+        let movieName = prompt("Один из последних просмотренных фильмов?");
+        let isNeedToAskAgain = (movieName === '') || (movieName.length > 50);
+    
+        if (isNeedToAskAgain){
+            alert("Невалидный ввод :( ");
+            return ask();
+        }
+    
+        let movieRating = prompt("На сколько оцените его?");
+    
+        return {movieName, movieRating};
+    }
+
+    toggleVisible(){
+        this.privat = (this.privat === true) ? (false) : (true);
+    }
 }
+
--- 1/js/script.js
+++ 1/js/script.js
@@ -5,69 +5,13 @@ import PersonalMovieDB from './PersonalMovieDB.js';
 let userDB = new PersonalMovieDB(0, {}, {}, [], false);
 
 
-start(userDB);
-detectPersonalLevel(userDB);
-rememberMyFilms(userDB);
-writeYourGenres(userDB);
-showMyDB(userDB);
+userDB.start();
+userDB.detectPersonalLevel();
+userDB.rememberMyFilms();
+userDB.writeYourGenres();
+userDB.showMyDB();
 
-function showMyDB(userDB){
-    if (userDB.privat == false)
-    {
-        console.log(userDB);
-    }
-}
 
-function writeYourGenres(userDB){
-    for (let i = 1; i <= 3; i++){
-        userDB.genres[i-1] = prompt("Ваш любимый жанр под номером "+ i);
-    }
-}
-
-function start(userDB){
-    userDB.count = prompt('Сколько фильмов вы уже посмотрели?', 2);
-}
-
-function rememberMyFilms(userDB){
-
-    for (let i = 0; i < userDB.count; i++){
-        let answer = ask();
-        let name = answer.movieName;
-        let rating = answer.movieRating;      
-
-        userDB.movies[name] = rating;
-    }
-}
-
-function detectPersonalLevel(userDB){
-    if (userDB.count < 10){
-        alert("Просмотрено довольно мало фильмов");
-    }
-    else if (userDB.count >= 10 && userDB.count < 30){
-        alert("Вы классический зритель");
-    }
-    else if (userDB.count >= 30){
-        alert("Вы киноман!");
-    }
-    else {
-        alert("Произошла ошибка!");
-    }
-}
-
-function ask(){
-    
-    let movieName = prompt("Один из последних просмотренных фильмов?");
-    let isNeedToAskAgain = (movieName === '') || (movieName.length > 50);
-
-    if (isNeedToAskAgain){
-        alert("Невалидный ввод :( ");
-        return ask();
-    }
-
-    let movieRating = prompt("На сколько оцените его?");
-
-    return {movieName, movieRating};
-}
 
 
 
